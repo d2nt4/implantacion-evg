f6f0228f2ef93c2140c2e81a92f887561c4738af
Daniel Torres <dtorresgalindo.guadalupe@alumnado.fundacionloyola.net>
2021-06-13 19:19:34 +0200

    Fallo de visualización de Modal de importar Secciones

--- src/application/views/C_Secciones/V_Secciones.php
+++ src/application/views/C_Secciones/V_Secciones.php
@@ -50,62 +50,62 @@
 					</div>
 				</div>
 			</div>
-			<?php
-				if(!is_null($this -> input -> cookie('importedSections')))
-				{
-					if($this -> input -> cookie('importedSections') != 0)
-					{
-						echo
-						'
-							<!--Bootstrap Import Modal-->
-							<div id="imports" class="modal fade" role="dialog" tabindex="-1" data-backdrop="static">
-								<div class="modal-dialog modal-dialog-centered" role="document">
-									<div class="modal-content">
-										<div class="modal-header">
-											<h5 class="modal-title font-weight-bolder">Importación Finalizada</h5>
-											<button type="button" class="close" data-dismiss="modal" aria-label="Close" onclick="eliminarModal()">
-												<span aria-hidden="true">&times;</span>
-											</button>
-										</div>
-										<div class="modal-body">';
-											echo "<p>Se han importado " . $this -> input -> cookie('importedSections') . ' secciones.</p>
-										</div>
-										<div class="modal-footer">
-											<button type="button" class="btn btn-danger font-weight-bolder" data-dismiss="modal" onclick="eliminarModal()">Cerrar</button>
-										</div>
-									</div>
-								</div>
-							</div>
-						';
-					}
-					else
-					{
-						echo
-						'
-							<!--Bootstrap Import Modal-->
-							<div id="imports" class="modal fade" role="dialog" tabindex="-1" data-backdrop="static">
-								<div class="modal-dialog modal-dialog-centered" role="document">
-									<div class="modal-content">
-										<div class="modal-header">
-											<h5 class="modal-title font-weight-bolder">Importación Finalizada</h5>
-											<button type="button" class="close" data-dismiss="modal" aria-label="Close" onclick="eliminarModal()">
-												<span aria-hidden="true">&times;</span>
-											</button>
-										</div>
-										<div class="modal-body">
-											<p>No se han importado secciones.</p>
-										</div>
-										<div class="modal-footer">
-											<button type="button" class="btn btn-danger font-weight-bolder" data-dismiss="modal" onclick="eliminarModal()">Cerrar</button>
-										</div>
-									</div>
-								</div>
-							</div>
-						';
-					}
-				}
-				delete_cookie('importedSections');
-			?>
 		</div>
+		<?php
+			if(!is_null($this -> input -> cookie('importedSections')))
+			{
+				if($this -> input -> cookie('importedSections') != 0)
+				{
+					echo
+					'
+								<!--Bootstrap Import Modal-->
+								<div id="imports" class="modal fade" role="dialog" tabindex="-1" data-backdrop="static">
+									<div class="modal-dialog modal-dialog-centered" role="document">
+										<div class="modal-content">
+											<div class="modal-header">
+												<h5 class="modal-title font-weight-bolder">Importación Finalizada</h5>
+												<button type="button" class="close" data-dismiss="modal" aria-label="Close" onclick="eliminarModal()">
+													<span aria-hidden="true">&times;</span>
+												</button>
+											</div>
+											<div class="modal-body">';
+					echo "<p>Se han importado " . $this -> input -> cookie('importedSections') . ' secciones.</p>
+											</div>
+											<div class="modal-footer">
+												<button type="button" class="btn btn-danger font-weight-bolder" data-dismiss="modal" onclick="eliminarModal()">Cerrar</button>
+											</div>
+										</div>
+									</div>
+								</div>
+							';
+				}
+				else
+				{
+					echo
+					'
+								<!--Bootstrap Import Modal-->
+								<div id="imports" class="modal fade" role="dialog" tabindex="-1" data-backdrop="static">
+									<div class="modal-dialog modal-dialog-centered" role="document">
+										<div class="modal-content">
+											<div class="modal-header">
+												<h5 class="modal-title font-weight-bolder">Importación Finalizada</h5>
+												<button type="button" class="close" data-dismiss="modal" aria-label="Close" onclick="eliminarModal()">
+													<span aria-hidden="true">&times;</span>
+												</button>
+											</div>
+											<div class="modal-body">
+												<p>No se han importado secciones.</p>
+											</div>
+											<div class="modal-footer">
+												<button type="button" class="btn btn-danger font-weight-bolder" data-dismiss="modal" onclick="eliminarModal()">Cerrar</button>
+											</div>
+										</div>
+									</div>
+								</div>
+							';
+				}
+			}
+			delete_cookie('importedSections');
+		?>
 	</body>
 </html>
